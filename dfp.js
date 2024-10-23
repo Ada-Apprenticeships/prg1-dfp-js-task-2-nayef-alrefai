dd402b70a827953a4430b07331cbf204f0c11c73
Nayef.Alrefai <159164377+nayef-alrefai@users.noreply.github.com>
2024-10-23 17:27:38 +0000

    added csv transformation functions

--- dfp.js
+++ dfp.js
@@ -1,6 +1,39 @@
 const fs = require('fs');
 
-function processData(data, delimiter = ';') {}
+const inputFile = './datafile.csv';
+const outputFile = './outputfile.csv';
+
+function processInput(datasheet, delimiter = ';'){
+  const lines = fs.readFileSync(datasheet, 'utf-8').split(/\n/);
+  const orderedLines = [];
+
+  for (const line of lines) {
+
+    const fields = line.split(delimiter);
+
+    const reversearray = fields.reverse();
+
+    const joinback = reversearray.join(delimiter);
+
+    orderedLines.push(joinback);
+  }
+  return orderedLines;
+}
+
+function cleanInput(orderedLines){
+  const cleanedLines = [];
+
+  for (let i=1; i < orderedLines.length; i++){
+    const line = orderedLines[i];
+
+    const trimmedLine = line.length > 20 ? line.slice(0,20) : line;
+
+    cleanedLined.push(trimmedLine)
+  }
+  return cleanedLines;
+}
+console.log(cleanedLines)
+function parseFile(indata, outdata, delimiter = ';') {}
 
 
 
