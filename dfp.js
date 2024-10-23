d3f4078cb84967052b66dfcae7c1edfe0d0e1912
Nayef.Alrefai <159164377+nayef-alrefai@users.noreply.github.com>
2024-10-23 09:58:45 +0000

    checks are ready

--- dfp.js
+++ dfp.js
@@ -1,8 +1,27 @@
 const fs = require('fs');
 
-function parseFile (indata, outdata, delimiter = ';') {
+const inputFile = "datafile.csv";
+const outputFile = "outputfile.csv";
 
+function parseFile (indata, outdata, delimiter = ';') {
+  //check if input file exists
+  const doesInputExist = fs.existsSync(indata) ? 'input file exists':'Input file does not exist';
+  console.log(doesInputExist);
+
+  //check if output file exists
+  if (fs.existsSync(outdata)) {
+    fs.unlinkSync(outdata);
+  }
+
+  //proceed with input file handling
+    if (fs.existsSync(indata)) {
+      // Read and process the input file
+      const data = fs.readFileSync(indata, "utf-8");
+      const lines = data.split("\n");
 }
+      const processedData = lines.map(line => line.split(delimiter).join(','));
+}
+console.log(parseFile(inputFile, outputFile))
 
 
 
